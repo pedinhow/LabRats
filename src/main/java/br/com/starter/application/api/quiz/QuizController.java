09595f242a6ce5bb97eb5f2f780173387c471fdc
Ricardo Marinho <ricardomarinho638@gmail.com>
2026-06-23 16:45:05 -0300

    feat: renomear quizzes

--- src/main/java/br/com/starter/application/api/quiz/QuizController.java
+++ src/main/java/br/com/starter/application/api/quiz/QuizController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import br.com.starter.application.api.common.ResponseDTO;
 import br.com.starter.application.api.quiz.dto.GenerateQuizRequestDTO;
 import br.com.starter.application.api.quiz.dto.GenerateQuizResponseDTO;
 import br.com.starter.application.api.quiz.dto.QuizResponseDTO;
+import br.com.starter.application.api.quiz.dto.UpdateQuizTitleRequestDTO;
 import br.com.starter.domain.quiz.Quiz;
 import br.com.starter.domain.quiz.QuizService;
 import br.com.starter.domain.user.CustomUserDetails;
@@ -59,6 +61,21 @@ public class QuizController {
             @PathVariable UUID quizId
     ) {
         Quiz quiz = quizService.findByIdAndCreator(quizId, userDetails.getUser());
+        return ResponseEntity.ok(new ResponseDTO<>(QuizResponseDTO.fromQuiz(quiz)));
+    }
+
+    @PatchMapping("/{quizId}/title")
+    public ResponseEntity<ResponseDTO<QuizResponseDTO>> rename(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable UUID quizId,
+            @RequestBody UpdateQuizTitleRequestDTO request
+    ) {
+        Quiz quiz = quizService.rename(
+                quizId,
+                request == null ? null : request.title(),
+                userDetails.getUser()
+        );
+
         return ResponseEntity.ok(new ResponseDTO<>(QuizResponseDTO.fromQuiz(quiz)));
     }
 }
